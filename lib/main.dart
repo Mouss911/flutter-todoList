c4225430425c7c36e665e50625e2208a074bebfe
Moussa Diatta <diattamoussa010@gmail.com>
2025-11-27 23:38:42 +0000

    fonction ajout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/todo_provider.dart';
+import 'screens/todo_list_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,115 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TodoListScreen(),
-    );
-  }
-}
-
-class Todo {
-  String title;
-  bool isDone;
-
-  Todo({required this.title, this.isDone = false});
-}
-
-class TodoListScreen extends StatefulWidget {
-  const TodoListScreen({super.key});
-
-  @override
-  _TodoListScreenState createState() => _TodoListScreenState();
-}
-
-class _TodoListScreenState extends State<TodoListScreen> {
-  final List<Todo> _todos = [];
-  final TextEditingController _textController = TextEditingController();
-
-  void _addTodo() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Nouvelle tâche'),
-          content: TextField(
-            controller: _textController,
-            decoration: InputDecoration(hintText: 'Entrez la tâche'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Annuler'),
-            ),
-            TextButton(
-              onPressed: () {
-                if (_textController.text.isNotEmpty) {
-                  setState(() {
-                    _todos.add(Todo(title: _textController.text));
-                  });
-                  _textController.clear();
-                }
-                Navigator.of(context).pop();
-              },
-              child: Text('Ajouter'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _toggleDone(int index) {
-    setState(() {
-      _todos[index].isDone = !_todos[index].isDone;
-    });
-  }
-
-  void _deleteTodo(int index) {
-    setState(() {
-      _todos.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ma Todo List'),
-      ),
-      body: ListView.builder(
-        itemCount: _todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Checkbox(
-              value: _todos[index].isDone,
-              onChanged: (value) {
-                _toggleDone(index);
-              },
-            ),
-            title: Text(
-              _todos[index].title,
-              style: TextStyle(
-                decoration: _todos[index].isDone ? TextDecoration.lineThrough : null,
-              ),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                _deleteTodo(index);
-              },
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addTodo,
-        child: Icon(Icons.add),
+    return ChangeNotifierProvider(
+      create: (context) => TodoProvider(),
+      child: MaterialApp(
+        title: 'Todo List',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const TodoListScreen(),
       ),
     );
   }
---
+++ lib/screens/todo_list_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/todo_provider.dart';
+
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Nouvelle tâche'),
+          content: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(hintText: 'Entrez la tâche'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_textController.text.isNotEmpty) {
+                  context.read<TodoProvider>().addTodo(_textController.text);
+                  _textController.clear();
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Ajouter'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ma Todo List'),
+      ),
+      body: Consumer<TodoProvider>(
+        builder: (context, todoProvider, child) {
+          return ListView.builder(
+            itemCount: todoProvider.todos.length,
+            itemBuilder: (context, index) {
+              final todo = todoProvider.todos[index];
+              return ListTile(
+                leading: Checkbox(
+                  value: todo.isDone,
+                  onChanged: (value) {
+                    todoProvider.toggleDone(index);
+                  },
+                ),
+                title: Text(
+                  todo.title,
+                  style: TextStyle(
+                    decoration:
+                        todo.isDone ? TextDecoration.lineThrough : null,
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    todoProvider.deleteTodo(index);
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
